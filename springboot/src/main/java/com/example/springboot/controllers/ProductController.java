974779dce9ec7f217706a11286d702832325f5e1
Rafael Teixeira <rafael.silva798@hotmail.com>
2023-12-19 01:08:11 -0300

    ENVIO

---
+++ springboot/src/main/java/com/example/springboot/controllers/ProductController.java
@@ -0,0 +1,90 @@
+package com.example.springboot.controllers;
+
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.model.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+
+@RestController
+public class ProductController {
+
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    //Passando a URI. Na hora que recebermos um POST vindo do cliente, esse método vai ser acionado.
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        //Inicialização do método. O retorno do método será o ResponseEntity do tipo ProductModel.
+        // O método se chama saveProduct que vai receber como corpo da solicitação HTTP via POST,
+        // o ProductRecordDto (Por isso precisa colocar a anotação RequestBody). Para que as validações
+        // @NotNull e @NotBlank entrem em vigor , é necessário incluir no início do método a anotação Valid,
+        // porque se não nenhuma anotação será feita.
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        List<ProductModel> productsList = productRepository.findAll();
+        if(!productsList.isEmpty()) {
+            for(ProductModel product : productsList) {
+                UUID id = product.getIdProduct();
+                product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products_List"));
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
+
+
+
+        @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value="id") UUID id) {
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if(productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        productRepository.delete(productO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
+                                                @RequestBody @Valid ProductRecordDto productRecordDto) {
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        var productModel = productO.get();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
+}
+
+
+
+
